fc226daf1b040077433534e12b4091fe80a3386c
Adam Cooper <adam@skout.com>
2016-04-25 16:09:03 -0700

    Initial Commit

---
+++ Animation Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Animation Test
+//
+//  Created by Adam Cooper on 4/25/16.
+//  Copyright © 2016 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Animation Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Animation Test
+//
+//  Created by Adam Cooper on 4/25/16.
+//  Copyright © 2016 Adam Cooper. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
